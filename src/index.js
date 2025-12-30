49e53134c3150db8daa33c84cfed57893fc2f98a
PacoGarcia2025 <sgoliveira16@gmail.com>
2025-12-30 11:01:37 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import extractDirecional from "./robots/direcional.extractor.js";
+import generateXmlX09 from "./xml/generateXml.x09.js";
+
+(async () => {
+  console.log("🚀 Iniciando robô Direcional");
+
+  const empreendimentos = await extractDirecional();
+
+  console.log(`📦 Total coletado: ${empreendimentos.length}`);
+
+  await generateXmlX09(empreendimentos);
+
+  console.log("✅ XML X09 gerado com sucesso");
+})();
 
